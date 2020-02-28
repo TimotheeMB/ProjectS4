110d3155cd6ad626a3bf37509511021337539bdb
Violaine <violaine.gzz@gmail.com>
2020-02-28 17:06:27 +0100

    non

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,2 +1,26 @@
 public class Obstacle {
+
+    // attributes
+    public int [] angle1 ;
+    public int [] angle2 ;
+
+
+    //constructor
+    public Obstacle (int angle1[], int angle2[]) {
+        this.angle1[] = angle1[] ;
+        this.angle2[] = angle2[] ;
+    }
+
+    public int [][] volumeObstacle (int angle1 [], int angle2[]) {
+        int [][] volume = new int [][] ;
+        volume [0][0] = {angle1[0], angle2[0]} ;
+        volume [1][1] = {angle1[1], angle2[1]} ;
+        return volume ;
+    }
+
+
+
+
+
+
 }
