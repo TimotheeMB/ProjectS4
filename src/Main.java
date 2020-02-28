cd1f3194954e54c898c9ff142c516c8707dd0ac7
Timothée <timothee.me@gmail.com>
2020-02-28 15:54:20 +0100

    Je repare les betises de Vivi

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class  Main {
 
     public static void main(String[] args) {
-        //Coucou Claire !
-        System.out.println("hello world");
-        System.out.println("j'ai rajouté kelkechossse");
+
     }
 }
-jnoiuhuio
