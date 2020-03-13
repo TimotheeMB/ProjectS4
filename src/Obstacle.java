6a66b2b58dc07becbd633d9a7f6c8f7b75b60e56
Violaine <violaine.gzz@gmail.com>
2020-03-13 17:41:55 +0100

    non

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,26 +1,54 @@
-public class Obstacle {
+public class Obstacle<x1, y1, x2, y2> {
 
     // attributes
-    public int [] angle1 ;
-    public int [] angle2 ;
 
+    public int x1 ;
+    public int y1 ;
+    public int x2 ;
+    public int y2 ;
 
     //constructor
-    public Obstacle (int angle1[], int angle2[]) {
-        this.angle1[] = angle1[] ;
-        this.angle2[] = angle2[] ;
+    public Obstacle (int x1, int y1, int x2, int y2) {
+        this.x1 = x1 ;
+        this.y1 = y1 ;
+        this.x2 = x2 ;
+        this.y2 = y2 ;
     }
 
-    public int [][] volumeObstacle (int angle1 [], int angle2[]) {
-        int [][] volume = new int [][] ;
-        volume [0][0] = {angle1[0], angle2[0]} ;
-        volume [1][1] = {angle1[1], angle2[1]} ;
-        return volume ;
+    //width of the rectangle on x
+    public int length (){
+        return (x2 - x1) ;
     }
 
+    //width of the rectangle on x
+    public int height (){
+        return (y2 - y1) ;
+    }
 
+    //set unavailable the rectangle occupied by the obstacle
+    public boolean [][] availableSpace (int x1, int y1, int x2, int y2) {
+        boolean[][] available = new boolean [this.length()][this.height()] ;
+        for (int i = x1 ; i<=x2 ; i++){
+            for (int j = y1 ; j<=y2 ; j++){
+                available[i][j] = false;
+            }
+        }
+        return available;
+    }
 
-
-
-
+    //create an obstacle (a dark rectangular shape)
+    /*public void actionPerformed (ActionEvent e){
+        fillrectangle(x1, y1, this.length(), this.height());
+    }*/
 }
+
+
+
+
+
+
+
+
+
+
+
