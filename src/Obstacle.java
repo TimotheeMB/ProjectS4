8a46b04a1086829dcd46e4c26658ffd7da62ec53
Violaine <violaine.gzz@gmail.com>
2020-03-13 18:00:40 +0100

    non

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,26 +1,35 @@
 public class Obstacle {
 
     // attributes
-    public int [] angle1 ;
-    public int [] angle2 ;
-
+    public int x1 ;
+    public int y1 ;
+    public int x2 ;
+    public int y2 ;
 
     //constructor
-    public Obstacle (int angle1[], int angle2[]) {
-        this.angle1[] = angle1[] ;
-        this.angle2[] = angle2[] ;
+    public Obstacle (int x1, int y1, int x2, int y2) {
+        this.x1 = x1 ;
+        this.y1 = y1 ;
+        this.x2 = x2 ;
+        this.y2 = y2 ;
     }
 
-    public int [][] volumeObstacle (int angle1 [], int angle2[]) {
-        int [][] volume = new int [][] ;
-        volume [0][0] = {angle1[0], angle2[0]} ;
-        volume [1][1] = {angle1[1], angle2[1]} ;
-        return volume ;
+    public int length (){
+        return (x2-x1) ;
     }
 
+    public int height (){
+        return (y2-y1) ;
+    }
 
-
-
-
+    public boolean [][] isAvailable (){
+        boolean [][] available = new boolean [this.length()][this.height()] ;
+        for (int i = this.x1 ; i<=this.x2 ; i++){
+            for (int j = this.y1 ; j<=this.y2 ; j++){
+                available [i][j] = false ;
+            }
+        }
+        return available ;
+    }
 
 }
