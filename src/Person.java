3796ae2edadf269857c7e89c443ac8a035646842
Timothée <timothee.me@gmail.com>
2020-03-30 18:55:17 +0200

    c'est carement propre...

--- src/Person.java
+++ src/Person.java
@@ -13,9 +13,6 @@ public class Person extends Entity{
     public ArrayList<Point> targets;
     int targetIndex;
 
-    //Obstacles
-    Obstacle currentObs=null;
-
     /* === CONSTRUCTOR === */
     public Person(Point center, Point target, Room room, int signature) {
 
@@ -23,7 +20,7 @@ public class Person extends Entity{
 
         //Position
         this.pos = around(center);
-        this.initPos = copyOf(pos);
+        this.initPos = pos;
 
         //Targets
         this.finalTarget=target;
@@ -36,24 +33,17 @@ public class Person extends Entity{
 
     public void move(){
 
-        System.out.println("This is my targetIndex : "+targetIndex);
-        System.out.println("This is my pos: "+pos[0]);
-
         removePrint();
-
         if(pos[0].equals(currentTarget())){
             targetIndex++;
-            System.out.println("je passe à la cible suivante");
         }
-
-        pos=around(pos[findCloserPoint(pos,currentTarget(),true)]);
-
+        pos=around(findCloserPoint(pos,currentTarget(),true,false));
         addPrint();
-
     }
 
     public void computeMyPathway() {
 
+        Obstacle currentObs=null;
         Point [] possibleTargets=new Point[4];
         int sign;
         int lastSign=0;
@@ -67,46 +57,52 @@ public class Person extends Entity{
                 currentObs.addPrint();
             }
 
-            pos=around(pos[findCloserPoint(pos,currentTarget(),false)]);
+            pos=around(findCloserPoint(pos,currentTarget(),false,false));
 
-
-            System.out.println("My imaginary position: "+pos[0]);
             sign = room.map[pos[0].x][pos[0].y];
-            System.out.println("J'imagine que je suis sur une case: "+sign);
 
             // if there is an obstacle
             if (sign != 0 && sign % 2 == 0) {
 
                 //if it a different obstacle
                 if(sign!=lastSign) {
-                    if(currentObs!=null) {
-                        currentObs.addPrint();
-                    }
                     currentObs = room.obstacles.get(sign / 2 - 1);
-                    System.out.println("il est #" + (sign / 2 - 1) + " dans la liste");
-                    possibleTargets=copyOf(currentObs.allPoints());
+                    possibleTargets=currentObs.allPoints();
                 }
-
 
                 currentObs.removePrint();
 
-                int pointToReach=findCloserPoint(possibleTargets,pos[0],false);
+                Point pointToReach=findCloserPoint(possibleTargets,pos[0],false,true);
 
-                targets.add(copyOf(possibleTargets[pointToReach])); //my target is point A of the obstacle on my way (c'est pas ce qu'on veut mais c'est un début)
-                possibleTargets[pointToReach]=new Point(10000,10000); //we "delete" the point from the possible targets
-                System.out.println("j'ai ajouter un nouvelle cible à ma liste'");
-                System.out.println("My new target: "+targets.get(targetIndex));
-
+                targets.add(pointToReach);
                 lastSign=sign;
             }
         }
 
-        if(currentObs!=null) {
-            currentObs.addPrint();
-        }
         targetIndex=0;
-        pos=copyOf(initPos);
+        pos=initPos;
         addPrint();
+    }
+
+    public Point findCloserPoint(Point[] points, Point target, boolean emptyPoint, boolean suppressThePoint){
+
+        double smallerDistance=points[0].distance(target); //default value, no check of emptiness
+        int index=0;
+
+        for (int i = 1; i < points.length ; i++) {
+            if(!emptyPoint||room.map[points[i].x][points[i].y]==0) {
+                double distance = points[i].distance(target);
+                if (distance < smallerDistance) {
+                    smallerDistance = distance;
+                    index=i;
+                }
+            }
+        }
+        Point r= points[index];
+        if(suppressThePoint) {
+            points[index] = new Point(10000, 10000);
+        }
+        return r;
     }
 
     public Point currentTarget(){
@@ -154,34 +150,4 @@ public class Person extends Entity{
                 new Point(p.x+1, p.y+2)
         };
     }
-
-    public Point[] copyOf(Point[] x){
-        Point [] r=new Point[x.length];
-        System.arraycopy(x, 0, r, 0, x.length);
-        return r;
-    }
-
-    public Point copyOf(Point p){
-        Point r=new Point(p.x,p.y);
-        return r;
-    }
-
-    public int findCloserPoint(Point[] points, Point target, boolean emptyPoint){
-
-        double smallerDistance=points[0].distance(target); //default value, no check of emptiness
-        int closerPoint=0;
-
-        for (int i = 1; i < points.length ; i++) {
-            if(!emptyPoint||room.map[points[i].x][points[i].y]==0) {
-                double distance = points[i].distance(target);
-                if (distance < smallerDistance) {
-                    smallerDistance = distance;
-                    closerPoint=i;
-                }
-            }
-        }
-
-        return closerPoint;
-    }
-
 }
